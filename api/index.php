ab424791bf7404ae474ef6b887e33ba2d1840da3
Jhonata Batista <jho.bat9@gmail.com>
2026-02-02 22:15:42 -0300

    Adiciona 'x-api-key' aos cabeçalhos permitidos para CORS

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, x-api-key");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
